43967f0aae10783f76d461e03f75ab83ac352a20
José Martins <jose.c.martins@sou.unifeob.edu.br>
2023-12-05 09:28:00 -0300

    category

---
+++ surse/database/migrations/CreateProductsTable.php
@@ -0,0 +1,35 @@
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained(); // Foreign key referencing categories table
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->boolean('exclusive')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
+git commit -m category
